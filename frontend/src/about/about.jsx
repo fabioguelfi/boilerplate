0a4ed8837b2832c7be683020c423dd4186c2993a
Fabio <fabio.guelfi@sgviagens.com.br>
2018-01-26 18:04:35 -0200

    continuar aula 52

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>lorem ipsum lorem ipsum</p>
+        <h2>Nossa Missão</h2>
+        <p>lorem ipsum lorem ipsum</p>
+        <h2>Nossos Valores</h2>
+        <p>lorem ipsum lorem ipsum</p>
     </div>
 )
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
 
+export default class Todo extends Component {
 
-class Todo extends Component {
+    handleAdd() {
+        console.log('Add')
+    }
+
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm handleAdd={this.handleAdd} />
+                <TodoList /> 
             </div>
         )
     }
 }
-
-export default Todo
